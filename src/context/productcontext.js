5c0652db4a0852eb4b6fed8497922c00e3084b0c
Ghazanfar Ali <gak_92@hotmail.com>
2023-02-05 18:07:23 +0500

    Add reducers to display data

--- src/context/productcontext.js
+++ src/context/productcontext.js
@@ -1,14 +1,29 @@
-import { createContext, useContext, useEffect } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
 import axios from "axios";
+import reducer from "../reducers/productReducer";
 
 const AppContext = createContext();
 const API = "https://api.pujakaitem.com/api/products";
+const initialState = {
+  isLoading: false,
+  isError: false,
+  products: [],
+  featureProducts: [],
+}; 
 
 const AppProvider = ({children}) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   const getProducst = async (url) => {
-    const res = await axios.get(url);
-    const products = await res.data;
-    console.log(products);
+    dispatch({type: "SET_LOADING"});
+    try {
+      const res = await axios.get(url);
+      const products = await res.data;
+      dispatch({type: "SET_API_DATA", payload: products});
+    }
+    catch(error) {
+      dispatch({type: "API_ERROR"});
+    }
   };
 
   useEffect(() => {
@@ -16,7 +31,7 @@ const AppProvider = ({children}) => {
   }, []);
 
   return (
-    <AppContext.Provider value={{myStoreName: "My Ecommerce Store2"}}>
+    <AppContext.Provider value={{...state}}>
       {children}
     </AppContext.Provider>
   );
