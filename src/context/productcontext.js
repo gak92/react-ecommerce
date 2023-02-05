358b12ba01b5e9c90e8a48ea5b32ea429b857462
Ghazanfar Ali <gak_92@hotmail.com>
2023-02-05 17:38:48 +0500

    fetch data from API using axios

--- src/context/productcontext.js
+++ src/context/productcontext.js
@@ -1,8 +1,20 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect } from "react";
+import axios from "axios";
 
 const AppContext = createContext();
+const API = "https://api.pujakaitem.com/api/products";
 
 const AppProvider = ({children}) => {
+  const getProducst = async (url) => {
+    const res = await axios.get(url);
+    const products = await res.data;
+    console.log(products);
+  };
+
+  useEffect(() => {
+    getProducst(API);
+  }, []);
+
   return (
     <AppContext.Provider value={{myStoreName: "My Ecommerce Store2"}}>
       {children}
