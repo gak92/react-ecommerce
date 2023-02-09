bb5d5297ba24556448b0171e57af8d9287c228c5
Ghazanfar Ali <gak_92@hotmail.com>
2023-02-09 14:57:47 +0500

    Structure products page with three new components

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,9 +1,42 @@
 import React from 'react'
+import FilterSection from './FilterSection';
+import ProductList from './ProductList';
+import Sort from './Sort';
+import styled from 'styled-components';
 
 const Products = () => {
   return (
-    <div>Products</div>
+    <Wrapper>
+      <div className='container grid grid-filter-column'>
+        {/* Filter column */}
+        <div>
+          <FilterSection />
+        </div>
+
+        {/* Product view section */}
+        <div className='product-view--sort'>
+          <div className='sort-filter'>
+            <Sort />
+          </div>
+
+          <div className='main-product'>
+            <ProductList />
+          </div>
+        </div>
+      </div>
+    </Wrapper>
   )
-}
+};
+
+const Wrapper = styled.section`
+  .grid-filter-column {
+    grid-template-columns: 0.2fr 1fr;
+  }
+  @media (max-width: ${({ theme }) => theme.media.mobile}) {
+    .grid-filter-column {
+      grid-template-columns: 1fr;
+    }
+  }
+`;
 
 export default Products
