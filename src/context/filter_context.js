77ea451cdeb04e17f6449e460529403cba8b3b8d
Ghazanfar Ali <gak_92@hotmail.com>
2023-02-10 15:19:08 +0500

    filter products based on input search field

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useReducer } from "react";
 import { useProductContext } from "./productcontext";
-import reducer from '../reducers/flterReducer';
+import reducer from "../reducers/flterReducer";
 
 const FilterContext = createContext();
 
@@ -8,41 +8,55 @@ const initialState = {
   filter_products: [],
   all_products: [],
   grid_view: true,
-  sorting_value: 'lowest',
+  sorting_value: "lowest",
+  filters: {
+    text: "",
+  },
 };
 
-export const FilterContextProvider = ({children}) => {
+export const FilterContextProvider = ({ children }) => {
   const { products } = useProductContext();
   // console.log(products);
 
   const [state, dispatch] = useReducer(reducer, initialState);
   // Set Grid View
   const setGridView = () => {
-    return dispatch({type: 'SET_GRID_VIEW'})
+    return dispatch({ type: "SET_GRID_VIEW" });
+  };
+
+  // Update the filter value
+  const updateFilterValue = (event) => {
+    let name = event.target.name;
+    let value = event.target.value;
+
+    return dispatch({ type: "UPDATE_FILTERS_VALUE", payload: { name, value } });
   };
 
   // Set Lsit View
   const setListView = () => {
-    return dispatch({type: 'SET_LIST_VIEW'})
+    return dispatch({ type: "SET_LIST_VIEW" });
   };
 
   // sorting function
   const sorting = (event) => {
     let userValue = event.target.value;
-    dispatch({type: 'GET_SORT_VALUE', payload: userValue})
+    dispatch({ type: "GET_SORT_VALUE", payload: userValue });
   };
 
   useEffect(() => {
-    dispatch({ type: 'LOAD_FILTER_PRODUCTS', payload: products})
+    dispatch({ type: "LOAD_FILTER_PRODUCTS", payload: products });
   }, [products]);
 
   // To sort the products
   useEffect(() => {
-    dispatch({type: 'SORTING_PRODUCTS'})
-  }, [state.sorting_value]);
-  
+    dispatch({ type: "FILTER_PRODUCTS" });
+    dispatch({ type: "SORTING_PRODUCTS" });
+  }, [products, state.sorting_value, state.filters]);
+
   return (
-    <FilterContext.Provider value={{...state, setGridView, setListView, sorting}}>
+    <FilterContext.Provider
+      value={{ ...state, setGridView, setListView, sorting, updateFilterValue }}
+    >
       {children}
     </FilterContext.Provider>
   );
@@ -50,4 +64,4 @@ export const FilterContextProvider = ({children}) => {
 
 export const useFilterContext = () => {
   return useContext(FilterContext);
-}
+};
--- src/reducers/flterReducer.js
+++ src/reducers/flterReducer.js
@@ -1,5 +1,5 @@
 const filterReducer = (state, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case "LOAD_FILTER_PRODUCTS":
       return {
         ...state,
@@ -36,26 +36,51 @@ const filterReducer = (state, action) => {
       const { filter_products, sorting_value } = state;
       let tempSortProducts = [...filter_products];
 
-      const sortingProducts = (a,b) => {
-        if(sorting_value === 'a-z') {
+      const sortingProducts = (a, b) => {
+        if (sorting_value === "a-z") {
           return a.name.localeCompare(b.name);
         }
-        if(sorting_value === 'z-a') {
+        if (sorting_value === "z-a") {
           return b.name.localeCompare(a.name);
         }
-        if(sorting_value === 'lowest') {
+        if (sorting_value === "lowest") {
           return a.price - b.price;
         }
-        if(sorting_value === 'highest') {
+        if (sorting_value === "highest") {
           return b.price - a.price;
         }
-      }
+      };
 
       newSortData = tempSortProducts.sort(sortingProducts);
       return {
         ...state,
         filter_products: newSortData,
       };
+
+    case "UPDATE_FILTERS_VALUE":
+      let { name, value } = action.payload;
+
+      return {
+        ...state,
+        filters: {
+          ...state.filters,
+          [name]: value,
+        },
+      };
+
+    case "FILTER_PRODUCTS":
+      let { all_products } = state;
+      let tempFilterProducts = [...all_products];
+      const {text} = state.filters;
+      if(text) {
+        tempFilterProducts = tempFilterProducts.filter((currElem) => {
+          return currElem.name.toLowerCase().includes(text);
+        });
+      }
+      return {
+        ...state,
+        filter_products: tempFilterProducts,
+      };  
 
     default:
       return state;
