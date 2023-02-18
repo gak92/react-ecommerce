f6dadadeaf5f35bc912225abeb5a92bbf8e7d229
Ghazanfar Ali <gak_92@hotmail.com>
2023-02-18 19:45:50 +0500

    Fix adding same product again to just increase amount and quantity rather add as new item

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -2,21 +2,48 @@ const cartReducer = (state, action) => {
   if(action.type === "ADD_TO_CART") {
     let {id, color, amount, product} = action.payload;
 
-    let cartProduct;
-    cartProduct = {
-      id: id+color,
-      name: product.name,
-      color,
-      amount,
-      image: product.image[0].url,
-      price: product.price,
-      max: product.stock,
-    }
+    // find if product exist already
+    let existingProduct = state.cart.find((currElem) => currElem.id === id+color);
 
-    return {
-      ...state,
-      cart: [...state.cart, cartProduct],
-    };
+    if(existingProduct) {
+      let updatedProduct = state.cart.map((currElem) => {
+        if(currElem.id === id+color) {
+          let newAmount =   currElem.amount + amount;
+          if(newAmount >= currElem.max) {
+            newAmount = currElem.max;
+          }
+          return {
+            ...currElem,
+            amount: newAmount,
+          };
+        }
+        else {
+          return currElem;
+        }
+      });
+
+      return {
+        ...state,
+        cart: updatedProduct,
+      };
+    }
+    else {
+      let cartProduct;
+      cartProduct = {
+        id: id+color,
+        name: product.name,
+        color,
+        amount,
+        image: product.image[0].url,
+        price: product.price,
+        max: product.stock,
+      }
+
+      return {
+        ...state,
+        cart: [...state.cart, cartProduct],
+      };
+    }    
   }
 
   if(action.type === "REMOVE_ITEM") {
