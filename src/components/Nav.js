3d18652d3f52a63f736e678e2ff83ab241fa7947
Ghazanfar Ali <gak_92@hotmail.com>
2023-02-24 18:36:01 +0500

    Add functionalit for login using auth0

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,6 +4,8 @@ import styled from "styled-components";
 import { FiShoppingCart } from "react-icons/fi";
 import { CgMenu, CgClose } from "react-icons/cg";
 import { useCartContext } from "../context/cart_context";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "../styles/Button";
 
 const Nav = () => {
   const Nav = styled.nav`
@@ -140,6 +142,7 @@ const Nav = () => {
 
   const [menuIcon, setMenuIcon] = useState();
   const { total_item } = useCartContext();
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
 
   return (
     <Nav>
@@ -181,6 +184,17 @@ const Nav = () => {
               Products
             </NavLink>
           </li>
+          {
+            isAuthenticated ?
+            <li>
+              <Button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+                Log Out
+              </Button>
+            </li> :
+            <li>
+              <Button onClick={() => loginWithRedirect()}>Log In</Button>
+            </li>
+          }
           <li>
             <NavLink
               to="/cart"
--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { AppProvider } from './context/productcontext';
-import { FilterContextProvider } from './context/filter_context';
-import { CartContextProvider } from './context/cart_context';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { AppProvider } from "./context/productcontext";
+import { FilterContextProvider } from "./context/filter_context";
+import { CartContextProvider } from "./context/cart_context";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppProvider>
-      <FilterContextProvider>
-        <CartContextProvider>
-          <App />
-        </CartContextProvider>
-      </FilterContextProvider>
-    </AppProvider>
+    <Auth0Provider
+      domain="dev-qtyyzze5xzq3ohz7.us.auth0.com"
+      clientId="0sCRyakbXEcfoev2JNaeuWlzbiyTAuwd"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <AppProvider>
+        <FilterContextProvider>
+          <CartContextProvider>
+            <App />
+          </CartContextProvider>
+        </FilterContextProvider>
+      </AppProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
